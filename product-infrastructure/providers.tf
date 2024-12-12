9648e7d5518c25aea4e081e3daad6becd38f69f0
Seundavid18 <oyewumidavid84@gmail.com>
2024-12-12 22:42:35 +0100

    updated terraform version and provider

--- product-infrastructure/providers.tf
+++ product-infrastructure/providers.tf
@@ -1,12 +1,17 @@
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+  subscription_id = "ceff4a75-79f1-4e77-8d92-31029c13d5f3"
 }
 
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "4.13.0"
     }
   }
 }
